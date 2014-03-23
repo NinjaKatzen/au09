816fb91993a017c21aefdbe98294aa201c62cbe8
niklas <niklas@localhost>
2014-03-23 23:34:13 +0100

    Initial Commit

---
+++ MyArrayList/src/dynamicData/MyArrayList.java
@@ -0,0 +1,206 @@
+package dynamicData;
+/**
+ * Stellt die Funktionalität einer ArrayList ohne Collections
+ * nach.
+ */
+public class MyArrayList {
+	
+	/**
+	 * Speichert die Daten der "Collection"
+	 */
+	private Object[] data;
+	
+	/**
+	 * 
+	 * @author Tobias Perny
+	 */
+	public MyArrayList(){
+		
+	}
+	
+	/**
+	 * 
+	 * @param o
+	 * @autor Tobias Perny
+	 */
+	public MyArrayList(Object[] o){
+		
+	}
+	
+	/**
+	 * 
+	 * @param size
+	 * @author Tobias Perny
+	 */
+	public MyArrayList(int size){
+		
+	}
+	
+	/**
+	 * 
+	 * @param o
+	 * @author Niklas Hohenwarter
+	 */
+	public boolean add(Object o){
+		return false;
+		
+	}
+	
+	/**
+	 * 
+	 * @param index
+	 * @param o
+	 * @author Niklas Hohenwarter
+	 */
+	public void add(int index, Object o){
+		
+	}
+	
+	/**
+	 * 
+	 * @author Inga Keilholz
+	 */
+	public void clear(){
+		
+	}
+	
+	/**
+	 * 
+	 * @param o
+	 * @return
+	 * @author Inga Keilholz
+	 */
+	public boolean contains(Object o){
+		return false;
+		
+	}
+	
+	/**
+	 * 
+	 * @param index
+	 * @return
+	 * @author Inga Keilholz
+	 */
+	public Object get(int index){
+		return index;
+		
+	}
+	
+	/**
+	 * 
+	 * @param o
+	 * @return
+	 * @author Tobias Perny
+	 */
+	public int indexOf(Object o){
+		return 0;
+		
+	}
+	
+	/**
+	 * 
+	 * @return
+	 * @author Inga Keilholz
+	 */
+	public boolean isEmpty(){
+		return false;
+		
+	}
+	
+	/**
+	 * 
+	 * @param o
+	 * @return
+	 * @author Selina Brinnich
+	 */
+	public int lastIndexOf(Object o){
+		return 0;
+	
+	}
+	
+	/**
+	 * 
+	 * @param index
+	 * @return
+	 * @author Selina Brinnich
+	 */
+	public Object remove(int index){
+		return index;
+		
+	}
+	
+	/**
+	 * 
+	 * @param o
+	 * @return
+	 * @author Selina Brinnich
+	 */
+	public boolean remove(Object o){
+		return false;
+		
+	}
+	
+	/**
+	 * 
+	 * @param o
+	 * @return
+	 * @author Niklas Hohenwarter
+	 */
+	public boolean removeAll(Object[] o){
+		return false;
+		
+	}
+	
+	/**
+	 * 
+	 * @param fromIndex
+	 * @param toIndex
+	 * @author Niklas Hohenwarter
+	 */
+	public void removeRange(int fromIndex, int toIndex){
+		
+	}
+	
+	/**
+	 * 
+	 * @param o
+	 * @return
+	 * @author Selina Brinnich
+	 */
+	public boolean retainAll(Object[] o){
+		return false;
+		
+	}
+	
+	/**
+	 * 
+	 * @param index
+	 * @param o
+	 * @return
+	 * @author Tobias Perny
+	 */
+	public Object set(int index, Object o){
+		return o;
+		
+	}
+	
+	/**
+	 * 
+	 * @return
+	 * @author Inga Keilholz
+	 */
+	public int size(){
+		return 0;
+		
+	}
+	
+	/**
+	 * Gibt das Array mit den Daten zurück.
+	 * @return Daten-Attribut
+	 * @author Niklas Hohenwarter
+	 */
+	public Object[] toArray(){
+		return data;	
+	}
+	
+}
